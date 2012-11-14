bae2416fd2e81c169b6085058bae74d826fc048e
Daniel Phillips <dyproepsilon@gmail.com>
2012-11-14 23:26:19 +1100

    semi-working org file playing now, sounds awful

--- music_player.c
+++ music_player.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include "SDL/SDL.h"
 #include "SDL/SDL_mixer.h"
+
+#include "organya.h"
  
 void create_tone(void *userdata, Uint8 *stream, int len);
 int sampler(signed char* samples, int length, double angle);
@@ -14,8 +16,7 @@ void read_samples();
 #define SPECIAL 3
 
 #define BEAT_SIZE 35
-//#define TUNING_NOTE 440
-#define TUNING_NOTE 220
+#define TUNING_NOTE 440
 
 #define A440 45
 
@@ -27,12 +28,21 @@ void read_samples();
 
 signed char *audio_samples[SAMPLES];
 
+organya_t* org;
+
 int main(int argc, char *argv[]) {
     /* Audio Setup */
     SDL_AudioSpec *desired, *obtained;
 
     read_samples();
+
+    if (argc <= 1) {
+        fprintf(stderr, "Must supply filename.\n");
+        return 1;
+    }        
     
+    org = organya_open(argv[1]);
+
     desired = (SDL_AudioSpec*)malloc(sizeof(SDL_AudioSpec));
     obtained = (SDL_AudioSpec*)malloc(sizeof(SDL_AudioSpec));
     
@@ -40,8 +50,7 @@ int main(int argc, char *argv[]) {
     desired->format=AUDIO_S16LSB;
 //    desired->format=AUDIO_S8;
     desired->channels=0;
-//    desired->samples=8192;
-    desired->samples=2048;
+    desired->samples=22050*org->wait_value/500;
     desired->callback=create_tone;
     desired->userdata=NULL;
  
@@ -65,49 +74,70 @@ int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
 }
 
-#include "music.h"
+//#include "music.h"
 
-int frequencies[NUM_TRACKS];
-double angles[NUM_TRACKS];
-int beat_upto[NUM_TRACKS];
-int note_upto[NUM_TRACKS];
+int frequencies[ORG_NUM_TRACKS];
+double angles[ORG_NUM_TRACKS];
+int resource_upto[ORG_NUM_TRACKS];
+
+unsigned int current_click = 0;
 
 void create_tone(void *userdata, Uint8 *stream, int len) {
     int i, j;
-    for (i = 0; i < NUM_TRACKS; i++) {
-        
+    for (i = 0; i < ORG_NUM_TRACKS/2; i++) {
+        track_t* cur_track = &org->tracks[i];
+        /*
         frequencies[i] = soundPitches[c_lengths[i] + note_upto[i]] != -128 ?
             TUNING_NOTE *
             pow(TEMPERAMENT,(float)(soundPitches[c_lengths[i] + note_upto[i]]-A440))
             : 0;
+        */
 
-        beat_upto[i] += 1;
-        if (beat_upto[i] >= soundDurations[c_lengths[i] + note_upto[i]]) {
-            beat_upto[i] = 0;
-            note_upto[i]++;
-            if (note_upto[i] >= lengths[i])
-                note_upto[i] = 0;
+        if (current_click >= cur_track->resources[resource_upto[i]].start +
+            cur_track->resources[resource_upto[i]].duration) {
+            if (resource_upto[i] < cur_track->num_resources - 1)
+                resource_upto[i]++;
+            else
+                continue;
+        }
+        if (current_click >= cur_track->resources[resource_upto[i]].start) {
+            frequencies[i] = TUNING_NOTE *
+                pow(TEMPERAMENT,
+                    (float)(cur_track->resources[resource_upto[i]].note - A440));
+        } else {
+            frequencies[i] = 0;
         }
     }
     
     for(i=0; i<len; i++) {
         *stream = 0;
-        for (j = 0; j < NUM_TRACKS; j++) {
-            *stream += sampler(audio_samples[instrument[j]],
+        for (j = 0; j < ORG_NUM_TRACKS/2; j++) {
+            track_t* cur_track = &org->tracks[j];
+            *stream += sampler(audio_samples[cur_track->instrument],
                                SAMPLE_LENGTH, angles[j]);
             
-            angles[j] += (PI/22050)*frequencies[j];
-            if(angles[j] >= 2.0*PI) {
-                angles[j] -= 2.0*PI;
+            angles[j] += (PI/22050)*frequencies[j]/2;
+            if (angles[j] >= 2.0*PI) {
+                angles[j] -=  2.0*PI;
+                if (angles[j] >= 2.0*PI) {
+                    angles[j] = 0; // Terrible hack,
+                    // getting huge angle values for some reason
+                }
             }
         }
         stream++;
     }
+
+    current_click++;
 }
 
 int sampler(signed char* samples, int length, double angle) {
     // Could overflow the array if angle >= (2*PI)
-    return samples[(int)(angle/(2*PI) * SAMPLE_LENGTH)];
+    if ((int)(angle/(2*PI) * length) >= length) {
+        return samples[length-1];
+    } else {
+        return samples[(int)(angle/(2*PI) * length)];
+    }
 }
 
 void read_samples() {
