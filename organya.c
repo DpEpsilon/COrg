4b53d23da0338262301591cd974c7dfbc400736b
Daniel Phillips <dyproepsilon@gmail.com>
2012-11-14 22:26:36 +1100

    added organya reader

---
+++ organya.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "organya.h"
+
+organya_t* organya_open(const char* filename) {
+    int t, r;
+    FILE* file = fopen(filename, "rb");
+    
+    organya_t *org = malloc(sizeof(organya_t));
+
+    fseek(file, 6, SEEK_CUR);
+    fread(&org->wait_value, 2, 1, file);
+    fseek(file, 2, SEEK_CUR);
+    fread(&org->loop_start, 4, 1, file);
+    fread(&org->loop_end, 4, 1, file);
+    
+    for (t = 0; t < ORG_NUM_TRACKS; t++) {
+        fseek(file, 2, SEEK_CUR);
+        fread(&org->tracks[t].instrument, 1, 1, file);
+        fseek(file, 1, SEEK_CUR);
+        fread(&org->tracks[t].num_resources, 2, 1, file);
+    }
+
+    for (t = 0; t < ORG_NUM_TRACKS; t++) {
+        org->tracks[t].resources =
+            malloc(sizeof(resource_t) * org->tracks[t].num_resources);
+        for (r = 0; r < org->tracks[t].num_resources; r++) {
+            fread(&org->tracks[t].resources[r].start, 4, 1, file);
+        }
+        for (r = 0; r < org->tracks[t].num_resources; r++) {
+            fread(&org->tracks[t].resources[r].note, 1, 1, file);
+        }
+        for (r = 0; r < org->tracks[t].num_resources; r++) {
+            fread(&org->tracks[t].resources[r].duration, 1, 1, file);
+        }
+        for (r = 0; r < org->tracks[t].num_resources; r++) {
+            fread(&org->tracks[t].resources[r].volume, 1, 1, file);
+        }
+        for (r = 0; r < org->tracks[t].num_resources; r++) {
+            fread(&org->tracks[t].resources[r].pan, 1, 1, file);
+        }
+    }
+    
+    return org;
+}
---
+++ organya.h
@@ -0,0 +1,30 @@
+#ifndef ORGANYA_H
+#define ORGANYA_H
+
+#define ORG_NUM_TRACKS 16
+
+typedef struct resource {
+    unsigned int start;
+    unsigned char note;
+    unsigned char duration;
+    unsigned char volume;
+    unsigned char pan;
+} resource_t;
+
+typedef struct track {
+    unsigned char instrument;
+    unsigned short num_resources;
+    resource_t* resources;
+} track_t;
+
+typedef struct organya {
+    unsigned short wait_value;
+    unsigned int loop_start;
+    unsigned int loop_end;
+    track_t tracks[ORG_NUM_TRACKS];
+} organya_t;
+
+organya_t* organya_open(const char* filename);
+//void organya_delete(organya_t* to_delete);
+
+#endif // ORGANYA_H
